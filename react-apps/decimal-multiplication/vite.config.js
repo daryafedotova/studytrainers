62e01b5d38f98e5e6651449018082082c84001c4
daryafedotova <dashkofedotka@gmail.com>
2026-09-11 22:21:45 +0300

    Добавить конфигурацию Vite для React-превью

---
+++ react-apps/decimal-multiplication/vite.config.js
@@ -0,0 +1,11 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+export default defineConfig({
+  base: "./",
+  plugins: [react(), viteSingleFile()],
+  build: {
+    target: "es2020"
+  }
+});
